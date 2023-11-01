4f7afe094fc29404795a1c3179c5aaacaba5a806
aristo-13 <oseirichmond310@gmail.com>
2023-11-01 13:34:58 +0000

    new

--- data/data.js
+++ data/data.js
@@ -1,14 +1,12 @@
 export const Data = [
-   
         { id: 1, name: "Classic Cheeseburger", category: "Burger", image: "https://images.pexels.com/photos/1556698/pexels-photo-1556698.jpeg?auto=compress&cs=tinysrgb&w=600", price: "$$" },
         { id: 2, name: "Pepperoni Pizza", category: "Pizza", image: "https://images.pexels.com/photos/1146760/pexels-photo-1146760.jpeg?auto=compress&cs=tinysrgb&w=600", price: "$" },
         { id: 3, name: "Grilled Chicken Tacos", category: "Tacos", image: "https://images.pexels.com/photos/616354/pexels-photo-616354.jpeg?auto=compress&cs=tinysrgb&w=600", price: "$$$" },
         { id: 4, name: "Caesar Salad", category: "Salad", image: "https://images.pexels.com/photos/7469432/pexels-photo-7469432.jpeg?auto=compress&cs=tinysrgb&w=600", price: "$$" },
         { id: 5, name: "Mushroom Swiss Burger", category: "Burger", image: "https://images.pexels.com/photos/3616956/pexels-photo-3616956.jpeg?auto=compress&cs=tinysrgb&w=600", price: "$$$$" },
-        { id: 6, name: "Margherita Pizza", category: "Pizza", image: "margherita_pizza.jpg", price: "$$" },
-        { id: 7, name: "Beef Burrito", category: "Burrito", image: "beef_burrito.jpg", price: "$$$" },
-        { id: 8, name: "Greek Salad", category: "Salad", image: "greek_salad.jpg", price: "$" },
-        { id: 9, name: "Veggie Wrap", category: "Wrap", image: "veggie_wrap.jpg", price: "$$" },
-        { id: 10, name: "BBQ Pulled Pork Sandwich", category: "Sandwich", image: "bbq_pulled_pork_sandwich.jpg", price: "$$$" },
-   
+        { id: 6, name: "Margherita Pizza", category: "Pizza", image: "https://images.pexels.com/photos/842519/pexels-photo-842519.jpeg?auto=compress&cs=tinysrgb&w=600", price: "$$" },
+        { id: 7, name: "Beef Burrito", category: "Burrito", image: "https://images.pexels.com/photos/2233729/pexels-photo-2233729.jpeg?auto=compress&cs=tinysrgb&w=600", price: "$$$" },
+        { id: 8, name: "Greek Salad", category: "Salad", image: "https://images.pexels.com/photos/1059905/pexels-photo-1059905.jpeg?auto=compress&cs=tinysrgb&w=600", price: "$" },
+        { id: 9, name: "Veggie Wrap", category: "Wrap", image: "https://images.pexels.com/photos/5852340/pexels-photo-5852340.jpeg?auto=compress&cs=tinysrgb&w=600", price: "$$" },
+        { id: 10, name: "BBQ Pulled Pork Sandwich", category: "Sandwich", image: "https://images.pexels.com/photos/12664777/pexels-photo-12664777.jpeg?auto=compress&cs=tinysrgb&w=600", price: "$$$" },
 ]
--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,31 +1,51 @@
 import { Data } from "../../data/data"
+import { useState } from "react"
 
 
 function Food() {
-    console.log(Data)
+const [foods,setFoods] = useState(Data)
+
+
+   /*  console.log(Data) */
   return (
-    <div>
-      <h1>Top Rated Menu Items</h1>
-      <div>
+    <div className="w-full p-4 py-12">
+      <h1 className="text-orange-600 font-bold text-3xl text-center">Top Rated Menu Items</h1>
+      <div className="flex flex-col lg:flex-row justify-between">
         <div>
-            <p>Filter Type</p>
-            <div>
-                <button>All</button>
-                <button>Burgers</button>
-                <button>Pizza</button>
-                <button>Salad</button>
-                <button>Chicken</button>
+            <p className="font-bold text-gray-700">Filter Type</p>
+            <div className="flex justify-between flex-wrap">
+                <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">All</button>
+                <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">Burgers</button>
+                <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">Pizza</button>
+                <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">Salad</button>
+                <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">Chicken</button>
             </div>
         </div>
         <div>
-            <p>Filter Price</p>
-            <div>
-                <button>$</button>
-                <button>$$</button>
-                <button>$$$</button>
-                <button>$$$$</button>
+            <p className="font-bold text-gray-700">Filter Price</p>
+            <div className="flex justify-between max-w-[390px] w-full">
+                <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">$</button>
+                <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">$$</button>
+                <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">$$$</button>
+                <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">$$$$</button>
             </div>
         </div>
+      </div>
+
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4">
+        {
+          foods.map((food,index) =>(
+            <div key={index} className="border shadow-lg rounded-lg hover:scale-105 duration-300 cursor-pointer">
+                <img className="w-full h-[200px] object-cover rounded-t-lg" src={food.image} alt={food.name} />
+                <div className="flex justify-between px-2 py-4">
+                    <p className="font-bold">{food.name}</p>
+                    <p>
+                        <span className="bg-orange-500 text-white p-1 rounded-full">{food.price}</span>
+                    </p>
+                </div>
+            </div>
+          ))
+        }
       </div>
     </div>
   )
